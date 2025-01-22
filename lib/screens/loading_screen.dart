2dd41d9c231fde36f37e66f6a4d914b35da8e6e4
Iniunam Idorenyin <iniunamid@gmail.com>
2025-01-22 13:14:05 +0000

    Added Geolocator package

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -8,6 +9,19 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  // final LocationSettings locationSettings = const LocationSettings(
+  //   accuracy: LocationAccuracy.low,
+  //   distanceFilter: 100
+  // );
+
+  void getLocation() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+    LocationPermission checkPermission = await Geolocator.checkPermission();
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +29,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: ElevatedButton(
           onPressed: () {
             //Get the current location
+            getLocation();
           },
           child: const Text('Get Location'),
         ),
